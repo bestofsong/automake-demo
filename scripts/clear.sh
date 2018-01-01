616560eef5322f9d8c13244a6e8e5b3af2dfaa7c
wansong <betterofsong@gmail.com>
2018-01-01 23:51:11 +0800

    rm no *.h, *.c, *.sh, *.cc files

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,6 +1,6 @@
-#! /bin/sh
+#! /usr/bin/env bash
 
-find . ! \( -name '*.h' -o -name '*.c' \) -type f -exec rm {} \;
+find . ! \( -name '*.h' -o -name '*.c' -o -name '*.sh' -o -name '*.cc' \) -type f ! -regex '^\./\.git/.*' -exec rm {} \;
 
-find . -type d -empty -exec test -d {} || rmdir {} \;
+find . -type d -empty ! -regex '^\./\.git/.*' -exec rmdir {} \;
 
