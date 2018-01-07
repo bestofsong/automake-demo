f2f3cccd589d5c212ad5fbabb6a4b12049eade61
wansong <betterofsong@gmail.com>
2018-01-07 14:22:09 +0800

    feat: use clang for cc, cxx

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/env bash
-
 # This script can be used to generate fat static libraries for ios from any c/c++ build system
 # The output flat libraries of lipo is in PREFIX/lipo/output
 #
 # Requirements for build system: configure at PROJ_ROOT
-# Requirements for placement of this script: PROJ_ROOT/<any script dir name>/<any script file name>
+# Requirements for placement of this script: PROJ_ROOT/<any script dir name>/<name of this script>
 #
 
 set -e
@@ -22,57 +21,50 @@ PROJ_ROOT=`dirname "${script_dir}"`
 BUILD_DIR=${PROJ_ROOT}/build
 CONFIGURE=${PROJ_ROOT}/configure
 
-LIPO_DIR=${BUILD_DIR}/lipo
 LIPO_INPUT_DIR=${BUILD_DIR}/lipo/input
 LIPO_OUTPUT_DIR=${BUILD_DIR}/lipo/output
 
 PREFIX=$BUILD_DIR
 LIB_DIR=${PREFIX}/lib
-INCLUDE_DIR=${PREFIX}/include
 
 [ -d "${BUILD_DIR}" ] && rm -rf "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
 
 
 do_build() {
-  export CC="$(xcrun -find -sdk ${SDK} cc)"
-  export CXX="$(xcrun -find -sdk ${SDK} cxx)"
-  export CPP="$(xcrun -find -sdk ${SDK} cpp)"
+  export CC="$(xcrun -find -sdk ${SDK} clang)"
+  export CXX="$(xcrun -find -sdk ${SDK} clang)"
+  export CPP="$(xcrun -find -sdk ${SDK} cpp)" #fixme: cannot put clang here?
   export CFLAGS="${HOST_FLAGS} ${OPT_FLAGS}"
   export CXXFLAGS="${HOST_FLAGS} ${OPT_FLAGS}"
   export LDFLAGS="${HOST_FLAGS}"
-
   "${CONFIGURE}" --host=${CHOST} --prefix=${PREFIX} --enable-static --disable-shared
   make && make install && make clean
 }
 
 do_collect_lib() {
-  for mod in `ls "${LIB_DIR}"` ; do
-    lipo_mod_dir=${LIPO_INPUT_DIR}/${mod}
-    [ -d "${lipo_mod_dir}" ] || mkdir -p "${lipo_mod_dir}"
-    lipo_mod_arch_dir=${lipo_mod_dir}/${ARCH}
-    mv "${LIB_DIR}/${mod}" "${lipo_mod_arch_dir}"
-  done
+  lipo_arch_dir=${LIPO_INPUT_DIR}/${ARCH}
+  [ -d "${LIPO_INPUT_DIR}" ] || mkdir -p "${LIPO_INPUT_DIR}"
+  mv "${LIB_DIR}" "${lipo_arch_dir}"
 }
 
 do_lipo() {
-  for mod in `ls "${LIPO_INPUT_DIR}"` ; do
-    lipo_mod_dir=${LIPO_INPUT_DIR}/${mod}
-    output_mod_dir=${LIPO_OUTPUT_DIR}/${mod}
-    [ -d "${output_mod_dir}" ] || mkdir -p "${output_mod_dir}"
+  arch_dir_names=(`ls "${LIPO_INPUT_DIR}"`)
+  any_arch_dir=${LIPO_INPUT_DIR}/${arch_dir_names[1]}
+  libs=`find "${any_arch_dir}" -name "*.a"`
+  prefix_len=${#any_arch_dir}
 
-    arch_dir_names=(`ls "${lipo_mod_dir}"`)
-    any_arch=${arch_dir_names[1]}
-    libs=`find "${lipo_mod_dir}/$any_arch" -name "*.a"`
-
-    for lib in $libs ; do
-      lib_name=`basename "${lib}"`
-      input_list=""
-      for arch in ${arch_dir_names[*]} ; do
-        input_list="${input_list} ${lipo_mod_dir}/${arch}/${lib_name}"
-      done
-      lipo -create -output "${output_mod_dir}/${lib_name}" $input_list
+  for lib in $libs ; do
+    relative_lib_name=${lib:${prefix_len}}
+    input_list=""
+    for arch in ${arch_dir_names[*]} ; do
+      input_list="${input_list} ${LIPO_INPUT_DIR}/${arch}/${relative_lib_name}"
     done
+
+    output_lib="${LIPO_OUTPUT_DIR}/${relative_lib_name}" 
+    output_lib_dir=`dirname "${output_lib}"`
+    [ -d "${output_lib_dir}" ] || mkdir -p "${output_lib_dir}"
+    lipo -create -output "${output_lib}" $input_list
   done
 }
 
